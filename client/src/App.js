ded28ca9e07f154005b0fac10050feade93e7300
Samolazoff <samolazov.roman.alex@gmail.com>
2024-10-21 14:06:54 +0300

    Add fonts and check fonts

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import './App.scss';
 function App() {
     return (
         <div className="App">
-        
+            <h1 className="one">qwewqe</h1>
+            <h1 className="two">qweqwe</h1>
+            <h1 className="three">qweqwe</h1>
         </div>
     );
 }
