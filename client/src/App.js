3f23119f539e391b29636d8a91223a63d1889f64
Samolazoff <samolazov.roman.alex@gmail.com>
2024-10-21 13:41:15 +0300

    Works with SASS

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+    return (
+        <div className="App">
+        
+        </div>
+    );
 }
 
 export default App;
