8cfb5194c851cc0b9878749a447e437a0543f217
Samolazoff <samolazov.roman.alex@gmail.com>
2024-11-02 19:37:26 +0300

    Add main disc box

---
+++ client/src/components/MainDis/MainDis.jsx
@@ -0,0 +1,16 @@
+import './MainDis.scss';
+
+function MainDis () {
+    return (
+        <section className="main-dis">
+            <h2 className="main-dis-title-box">
+                World of books
+            </h2>
+            <p className="main-dis-txt">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus dolorem blanditiis asperiores fugiat, doloremque dignissimos sequi accusantium maxime, explicabo excepturi praesentium. Animi et, recusandae voluptatem non ut voluptatibus cupiditate reiciendis!
+            </p>
+        </section>
+    )
+};
+
+export default MainDis;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import './Home.scss';
 
 import BgcMain from '../../components/BgcMain/BgcMain';
+import MainDis from '../../components/MainDis/MainDis';
 
 function Home () {
     return (
         <main className="app-main-home">
             <BgcMain/>
+            <MainDis/>
         </main>
     )
 }
