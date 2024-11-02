82efc291f3074f306acdab12a8bf9ca9d57ffd9a
Samolazoff <samolazov.roman.alex@gmail.com>
2024-11-02 14:43:48 +0300

    Add  style for login

--- client/src/components/NavLogin/NavLogin.jsx
+++ client/src/components/NavLogin/NavLogin.jsx
@@ -5,7 +5,7 @@ import {NavLink} from 'react-router-dom';
 function NavLogin (props) {
     const data=props.dataset;
     return (
-        <li className='navbar-item'>
+        <li className='navbar-item navbar-item__login'>
             <NavLink to={data.href} key={data.href}> {data.name}</NavLink>
         </li>
     )
