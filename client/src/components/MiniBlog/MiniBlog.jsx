e240cebfec43127bff2e7619d64e2d958cc4dd73
Samolazoff <samolazov.roman.alex@gmail.com>
2024-11-02 20:10:28 +0300

    Create box mini-blog

---
+++ client/src/components/MiniBlog/MiniBlog.jsx
@@ -0,0 +1,13 @@
+import '/MiniBlog.scss';
+
+function MiniBlog () {
+    return (
+        <aside className="mini-blog">
+            <h2 className="mini-blog-title">
+                Blog
+            </h2>
+        </aside>
+    )
+};
+
+export default MiniBlog;
