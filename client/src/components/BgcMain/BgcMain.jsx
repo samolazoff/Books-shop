7aba8804b9fe275e2d0e05ff4a44f0206343d9e8
Samolazoff <samolazov.roman.alex@gmail.com>
2024-11-02 18:47:51 +0300

    Add bgc box for main

---
+++ client/src/components/BgcMain/BgcMain.jsx
@@ -0,0 +1,10 @@
+import './BgcMain.scss';
+
+function BgcMain() {
+    return(
+        <div className="bgc-main">
+        </div>
+    )
+}
+
+export default  BgcMain;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,11 @@
-import './Home.scss'
+import './Home.scss';
+
+import BgcMain from '../../components/BgcMain/BgcMain';
 
 function Home () {
     return (
         <main className="app-main-home">
-            
-
+            <BgcMain/>
         </main>
     )
 }
