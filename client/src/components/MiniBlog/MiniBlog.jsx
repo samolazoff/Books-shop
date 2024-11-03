ae3abc559d204c18177a56e7612a2985a3026955
Samolazoff <samolazov.roman.alex@gmail.com>
2024-11-03 12:30:29 +0300

    Create conseption aside

--- client/src/components/MiniBlog/MiniBlog.jsx
+++ client/src/components/MiniBlog/MiniBlog.jsx
@@ -1,4 +1,4 @@
-import '/MiniBlog.scss';
+import './MiniBlog.scss';
 
 function MiniBlog () {
     return (
@@ -6,6 +6,12 @@ function MiniBlog () {
             <h2 className="mini-blog-title">
                 Blog
             </h2>
+            <ul className='mini-blog-news'>
+                <li className='mini-blog-news_items'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid corrupti quisquam quasi officia aut odit, quaerat illo. Delectus fugit error temporibus? Asperiores ab quam molestias animi commodi, provident facere aperiam.</li>
+                <li className='mini-blog-news_items'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid corrupti quisquam quasi officia aut odit, quaerat illo. Delectus fugit error temporibus? Asperiores ab quam molestias animi commodi, provident facere aperiam.</li>
+                <li className='mini-blog-news_items'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid corrupti quisquam quasi officia aut odit, quaerat illo. Delectus fugit error temporibus? Asperiores ab quam molestias animi commodi, provident facere aperiam.</li>
+                <li className='mini-blog-news_items'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid corrupti quisquam quasi officia aut odit, quaerat illo. Delectus fugit error temporibus? Asperiores ab quam molestias animi commodi, provident facere aperiam.</li>                
+            </ul>
         </aside>
     )
 };
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,12 +2,14 @@ import './Home.scss';
 
 import BgcMain from '../../components/BgcMain/BgcMain';
 import MainDis from '../../components/MainDis/MainDis';
+import MiniBlog from '../../components/MiniBlog/MiniBlog';
 
 function Home () {
     return (
         <main className="app-main-home">
             <BgcMain/>
             <MainDis/>
+            <MiniBlog/>
         </main>
     )
 }
